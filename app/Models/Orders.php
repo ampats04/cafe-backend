b87d535feebd19d6dabdb3a400a939ac03b5f0d8
ampats04 <jeremyandyampatin@gmail.com>
2024-09-14 00:22:39 +0800

    model(order): created order model

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -8,4 +8,46 @@ use Illuminate\Database\Eloquent\Model;
 class Orders extends Model
 {
     use HasFactory;
+
+    protected $table = 'orders';
+    protected $primaryKey = 'pkOrderId';
+
+
+    protected $fillable = [
+        'fkTableId',
+        'fkProductId',
+        'quantity',
+        'status',
+    ];
+
+    public function table()
+    {
+
+        return $this->belongsTo(Table::class, 'fkTableId', 'pkTableId');
+    }
+
+    public function product()
+    {
+
+        return $this->belongsTo(Product::class, 'fkProductId', 'pkProductId');
+    }
+
+
+    public function scopeActive($query)
+    {
+
+        return $query->where('status', '=', 'Active');
+    }
+
+    public function scopePending($query)
+    {
+
+        return $query->where('status', '=', 'Pending');
+    }
+
+    public function scopeCompleted($query)
+    {
+
+        return $query->where('status', '=', 'Completed');
+    }
 }
