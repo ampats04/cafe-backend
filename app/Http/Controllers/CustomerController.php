a2ed24724dce5327a932b8ea70f1f3add19ec775
ampats04 <jeremyandyampatin@gmail.com>
2024-09-14 00:21:12 +0800

    feat(customer): added function for adding customername

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TableRequest;
+use App\Models\Table;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function addCustomerName(TableRequest $request)
+    {
+
+        $tableId = session('tableId');
+
+
+        if (!session()->has('tableId')) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'No sesions found for ' . $tableId
+            ]);
+        }
+
+        $table = Table::find($tableId);
+        if (!$table) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Table not found. Please double-check your table number.'
+            ], 404); // Not found
+        }
+
+        $table->customer_name = $request->customerName;
+        $table->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Customer name updated successfully.',
+            'table' => $table
+        ], 200); // Success
+    }
+
 }
