39c90fc3a6d4134ae44f2d43babc0e4316345553
ampats04 <jeremyandyampatin@gmail.com>
2024-09-14 00:24:29 +0800

    migration(orders): create order migration

---
+++ database/migrations/2024_08_20_153247_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('pkOrderId');
+            $table->unsignedBigInteger('fkTableId');
+            $table->unsignedBigInteger('fkProductId');
+            $table->integer('quantity')->default(0);
+            //Active = Inside Cart
+            //Pending = Checkout
+            //Completed = Served
+            $table->enum('status', ['Active', 'Pending', 'Completed']);
+            $table->foreign('fkTableId')->references('pkTableId')->on('tables')->onDelete('cascade');
+            $table->foreign('fkProductId')->references('pkProductId')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
