a4e68a14104e8d3d6de71fc5b561ed371e84de33
ampats04 <jeremyandyampatin@gmail.com>
2024-09-14 00:20:37 +0800

    migration(tables): create tables migration

---
+++ database/migrations/2024_08_20_153246_create_tables_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id('pkTableId');
+            $table->integer('tableNumber');
+            $table->string('customerName')->nullable();
+            $table->enum('status', ['Active', 'Inactive'])->default('Inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
