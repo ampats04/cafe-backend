16ed56a711395667a9d78e2e71e62ee44e298645
ampats04 <jeremyandyampatin@gmail.com>
2024-09-17 19:55:02 +0800

    fix(cors): error in credentials

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -23,11 +23,14 @@ class Cors
             $response->headers->set('Access-Control-Allow-Origin', '*');
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             $response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, Accept');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
         } else {
             $response->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, Accept');
+                ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, Accept')
+                ->header('Access-Control-Allow-Credentials', 'true');
         }
+
 
         return $response;
     }
