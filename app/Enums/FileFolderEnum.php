51ec7c54d6f2d10dcd7b43d1c0ae334e92b4ed29
ampats04 <jeremyandyampatin@gmail.com>
2024-09-18 19:36:59 +0800

    feat(enums): added folder enum for storing images

--- app/Enums/FileFolderEnum.php
+++ app/Enums/FileFolderEnum.php
@@ -4,10 +4,5 @@ namespace App\Enums;
 
 enum FileFolderEnum: string
 {
-    case DisplayPicture = "displayPictures";
-    case AdminDisplayPicture = "displayPictures/admin";
-    case Documents = "documents";
-    case SKID = "skid";
     case Products = "products";
-    case Posts = "posts";
 }
