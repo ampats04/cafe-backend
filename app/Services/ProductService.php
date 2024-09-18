3833c5ced976ae4d5dbb91623f29c5c304e7c58b
ampats04 <jeremyandyampatin@gmail.com>
2024-09-18 19:38:20 +0800

    feat(products): added productImage logic and storing

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -1,14 +1,32 @@
 <?php
 namespace App\Services;
 
+use App\Enums\FileFolderEnum;
 use App\Http\Requests\ProductRequest;
 use App\Models\Foods;
 use App\Models\Product;
+use Carbon\Carbon;
 use Exception;
+use Storage;
+use Str;
 
 class ProductService
 {
 
+    public function storeImage($img, $product_name, $folder_name)
+    {
+        try {
+            //code...
+            $currentDateTime = Carbon::now()->format('Ymd_His');
+            $newFilename = 'document_' . $currentDateTime . Str::random(10) . '.' . $img->getClientOriginalExtension();
+            $path = 'images/' . $folder_name . '/' . $product_name . '/' . $newFilename;
+            // $filePath = $path;
+            Storage::disk('local')->put('public/' . $path, file_get_contents($img), 'public');
+            return $path;
+        } catch (Exception $th) {
+            throw $th;
+        }
+    }
     public function addProduct($request)
     {
         try {
@@ -22,10 +40,19 @@ class ProductService
                 'availability' => isset($request->availability) ? $request->availability : 'Not Available'
             ]);
 
+            if ($foods) {
+
+                if ($request->hasFile('productImage')) {
+                    $path = $this->storeImage($request->file('productImage'), $foods->pkProductId, FileFolderEnum::Products->value);
+                    $foods->productImage = $path;
+                }
+                $foods->save();
+            }
+
             return $foods;
 
         } catch (Exception $e) {
-            return $e;
+            throw $e;
         }
     }
 
